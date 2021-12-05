c54a27adfcb666aa1a08f282ec8b97005f02c864
vandana13mehta <vandana13mehta@gmail.com>
2021-12-05 11:46:33 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("/")]
+    //Controller
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string myname)
+        {
+            ViewBag.Message = "Hello , " + myname;
+            return View();
+        }
+
+
+/*
+        [HttpGet]
+        [Route("/about")]
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AboutMe(int salary)
+        {
+            string view="";
+            if(salary == 0)
+			{
+                 view = "Empty";
+			}
+            ViewBag.Message = "Your salary is" + view;
+            return View();
+        }
+
+*/
+
+    }
+    
+   
+}
